8fa9bdcfcd4c128c68a744d122a8d7a9a6214cd2
Jose Guerra <jose.guerra@dodsgroup.com>
2021-11-26 13:20:55 +0000

    removed console log

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = ({ feed }) => {
-  console.log(feed);
   return (
     <div className={styles.container}>
       <Head>
